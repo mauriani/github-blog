125664e8620c5d1c54b280da8c178fb83626ff3b
Mauriani Maciel <maurianim@gmail.com>
2022-10-09 09:58:28 -0300

    first commit

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+import BackgroundImage from "../../assets/Cover.png";
+
+export const Container = styled.header`
+  width: 100%;
+  height: 296px;
+  background: url(${BackgroundImage}) no-repeat top center;
+  background-size: 100%;
+`;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../../components/Header";
+import { Container } from "./styles";
+export function Home() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
